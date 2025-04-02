3d063eb955f81ebb3030a20b9d91dc7f9f65cf88
august roosi <augustroosi@gmail.com>
2025-04-02 19:03:43 +0300

    Removed argument map creation in react flow view.

--- argpuu/argument_map_app/static/frontend/src/App.tsx
+++ argpuu/argument_map_app/static/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import {
   Controls,
   Background,
 } from '@xyflow/react';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 
 import '@xyflow/react/dist/style.css';
@@ -36,7 +36,7 @@ const selector = (state: AppState) => ({
 function Flow({ argumentMapId }: FlowProps) {
 
 
-  const { nodes, edges, onNodesChange, onEdgesChange, onConnect, getMapNodes, createNode } = useStore(
+  const { nodes, edges, onNodesChange, onEdgesChange, onConnect, getMapNodes } = useStore(
     useShallow(selector),
   );
 
@@ -45,70 +45,16 @@ function Flow({ argumentMapId }: FlowProps) {
     console.log("Flow component has mounted.");
     console.log("Argument Map ID:", argumentMapId);
     if (argumentMapId) {
-      setIsPopupOpen(false); 
       getMapNodes(argumentMapId);
     }
   }, [argumentMapId]); 
   
-  
-  const [isPopupOpen, setIsPopupOpen] = useState(true); // Popup state
-  const [nodeName, setNodeName] = useState(''); // Node name input
 
 
-  const handleKeyDown = (e: any) => {
-    if (e.key === 'Enter') {
-      handleCreateNode();
-    }
-  };
-
-  const handleCreateNode = () => {
-    if (nodeName.trim()) {
-      createNode(nodeName, true); 
-      setIsPopupOpen(false); 
-    }
-  };
 
 
   return (
     <div style={{ height: '100%' }}>
-      {isPopupOpen && nodes.filter(node => 'isTopic' in node.data && node.data.isTopic).length === 0 && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-xs">
-          <div
-            className="rounded-lg shadow-lg p-6 max-w-md w-full bg-stone-200">
-            <h3 className="text-lg font-bold text-gray-800 mb-4 ">Kirjuta enda väide</h3>
-
-            <div className="flex justify-between items-center mb-4 gap-2">
-              
-
-
-              <input
-                type="text"
-                placeholder="Kirjuta enda väide"
-                value={nodeName}
-                onKeyDown={handleKeyDown}
-                onChange={(e) => setNodeName(e.target.value)}
-                className="w-full m-0 px-4 py-2 text-gray-800 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 hover:ring-1 focus:outline-none transition-all duration-300"
-              />
-            </div>
-            <div className="flex justify-end gap-2">
-              <button
-                onClick={handleCreateNode}
-                className="text-gray-600  px-4 py-2 rounded-md bg-green-600 hover:bg-opacity-50 focus:ring-2 focus:ring-green-300 focus:outline-none transition-all duration-300"
-              >
-                Valmis
-              </button>
-
-
-              <button
-                onClick={() => setIsPopupOpen(false)}
-                className="text-gray-600 text-white px-4 py-2 rounded-md bg-yellow-600 hover:bg-opacity-50 focus:ring-2 focus:ring-red-300 focus:outline-none transition-all duration-300"
-              >
-                Tagasi
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
   
       <ReactFlow 
       nodes={nodes}
