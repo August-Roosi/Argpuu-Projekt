0100a3c6bd6d8418a211a8b5973a74d3bdfcb0c2
august roosi <augustroosi@gmail.com>
2025-05-12 19:24:44 +0300

    Max connection age added.

--- argpuu/argpuu/settings.py
+++ argpuu/argpuu/settings.py
@@ -101,7 +101,8 @@ DATABASES = {
     # )
 
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
     )
 
     # 'default': {
