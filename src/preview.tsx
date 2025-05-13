345ea4d50b78ee5579f99958346119aa97f27a44
august roosi <augustroosi@gmail.com>
2025-05-13 14:44:10 +0300

    New react flow on home page.

---
+++ src/preview.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+    ReactFlow,
+    Background,
+    Edge,
+} from '@xyflow/react';
+import 'reactflow/dist/style.css';
+import { AppNode } from './nodes/types';
+
+const nodes: AppNode[] = [
+    {
+        id: '1',
+        data: { label: 'Kõik lumememmed on valged' },
+        position: { x: 0, y: 0 },
+        type: 'argument-node',
+    },
+        {
+        id: '2',
+        data: { label: 'Kõik lumi on valge' },
+        position: { x: 50, y: 100 },
+        type: 'argument-node',
+        extent: 'parent',
+        parentId: '4',
+    },
+        {
+        id: '3',
+        data: { label: 'Kõik lumememmed on lumest' },
+        position: { x: 0, y: 100 },
+        type: 'argument-node',
+        extent: 'parent',
+        parentId: '4',
+    },
+        {
+        id: '4',
+        position: { x: 0, y: 100 },
+        data: {},
+        type: 'operator-node',
+    },
+];
+
+const edges: Edge[] = [];
+
+ReactDOM.createRoot(document.getElementById('preview-root')!).render(
+    <React.StrictMode>
+        <div style={{ height: 400 }}>
+            <ReactFlow nodes={nodes} edges={edges} />
+        </div>
+    </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 export default defineConfig({
   root: './', // Ensure the root is set correctly if `index.html` is not in the root
@@ -7,10 +9,14 @@ export default defineConfig({
   build: {
     manifest: true,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        preview: path.resolve(__dirname, 'preview.html'),
+      },
       output: {
-        entryFileNames: 'assets/[name].js', // No hash in entry file names
-        chunkFileNames: 'assets/[name].js', // No hash in chunk files
-        assetFileNames: 'assets/[name][extname]', // No hash in other assets
+        entryFileNames: 'assets/[name].js', 
+        chunkFileNames: 'assets/[name].js', 
+        assetFileNames: 'assets/[name][extname]', 
       },
     },
   },
